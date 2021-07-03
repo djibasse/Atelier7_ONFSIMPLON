444e6d9469cd084da8588c5a3b58ebff72a9951f
djibasse <83929893+djibasse@users.noreply.github.com>
2021-07-03 15:44:32 +0000

    conflict

--- src/Form/ApprenantType.php
+++ src/Form/ApprenantType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ApprenantformType extends AbstractType
+class ApprenantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
